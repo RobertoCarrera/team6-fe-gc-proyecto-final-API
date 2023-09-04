b1f1036e3a20b2c72086ecbe544c6f548bcc8d76
RobertoCarrera <robertocarrera93@gmail.com>
2023-09-04 15:23:55 +0200

    Corrección para Railway- Faltaban 3 archivos principales

---
+++ src/main/java/com/jetxperience/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jetxperience;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(API_JetXpeRienceApplication.class);
+	}
+
+}
